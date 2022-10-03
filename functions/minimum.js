bc784f7ad351096ed4c5f16062f3b5c225ed3a50
Preston Hogaboam <pjhogaboam@gmail.com>
2022-10-03 22:21:22 +0200

    created a min function that takes two arguments and returns the smaller

---
+++ functions/minimum.js
@@ -0,0 +1,7 @@
+// we want to write a function that takes two arguments and returns their minimum.
+const minimize = (num1, num2) => {
+  return console.log(Math.min(num1, num2));
+}
+
+// test case should return 7
+minimize(7,9);
